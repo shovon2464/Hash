47c65dd3d4606beffd22341edc86bcae9a3f5819
shovon2464 <shovon2464@gmail.com>
2021-03-31 19:56:58 +0600

    problem 1 is solved with an array of charset using boolean

--- src/IsUnique.java
+++ src/IsUnique.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class IsUnique {
+import java.util.HashSet;
+
+public class IsUnique {
+    public static void main(String [] args){
+        String test = "Shovn";
+        HashSet<Character> testSet = new HashSet<Character>();
+        for(int i=0; i<test.length();i++){
+            if(testSet.contains(test.charAt(i))){
+                System.out.println("Yes");
+            }
+            else{
+                testSet.add(test.charAt(i));
+            }
+        }
+        System.out.println("No");
+    }
 }
--- src/IsUnique2.java
+++ src/IsUnique2.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class IsUnique2 {
+public class IsUnique2 {
+    static boolean isUniqueChars(String str){
+        if(str.length()>128){
+            return false;
+        }
+        boolean [] char_set = new boolean [128];
+        for(int i=0;i<str.length();i++){
+            int val = str.charAt(i);
+            if(char_set[val] == true){
+                return false;
+            }
+            else{
+                char_set[val] = true;
+            }
+        }
+        return true;
+    }
+    public static void main(String [] args){
+
+        String str = "shovon";
+        boolean x = isUniqueChars(str);
+        System.out.println(x);
+
+
+    }
 }
